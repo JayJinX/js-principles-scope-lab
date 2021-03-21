d9b59acbbfdd0d136185333f839a6733e2049d02
HuangjieJin <jinhuangjie233@gmail.com>
2021-03-21 22:45:36 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    return customerName = "BOB";
+}
+
+setBestCustomer = () => bestCustomer = "not bob";
+
+overwriteBestCustomer = () => bestCustomer = "maybe bob";
+
+const leastFavoriteCustomer = "pig";
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "cat";
+}
